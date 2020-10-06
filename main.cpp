a78572780bffbaea011f993a112036d1ae4de7e4
BoboMac <30493004+BoboMac@users.noreply.github.com>
2020-10-06 12:21:34 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "output.h"
+#include "backend.h"
+
+int main(int argc, char **argv) {
+
+    // Initialize I/O
+
+    // Main loop
+
+    // Terminate and return to terminal
+    
+    return 0;
+}
